8e172cf6b948cd81e29582b20da2370f745369f5
Jado <jade.bouiges@edu.esiee-it.fr>
2024-12-05 19:50:39 +0100

    add

---
+++ Assets/Scripts/HoverButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoverButton : MonoBehaviour
+{
+    [SerializeField] GameObject _button;
+    [SerializeField] GameObject _cadreButton;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnPointExite(PointerEventData eventData)
+    {
+
+    }
+}
+
+
+
+
